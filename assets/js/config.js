ec923fcd6620975140b1ae903d6eef9bc48b832d
QuantiumCodeV <quantiumcode@mail.ru>
2025-08-06 22:16:26 +0300

    fix

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+const config = {
+    x: "https://chromewebstore.google.com/detail/pflemlblnedmieclkkdlependelghmlo?utm_source=item-share-cb)",
+    tg: "https://chromewebstore.google.com/detail/pflemlblnedmieclkkdlependelghmlo?utm_source=item-share-cb)",
+}
+
+
